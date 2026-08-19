3d8234d1c7c2100001388c94892c430a29319edf
Yassin Marmoud <ymarmoud@gmail.com>
2026-08-19 16:03:53 +0200

    fix the img errors

--- script.js
+++ script.js
@@ -551,7 +551,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "img/balkone/balkone.jpeg",
     "img/balkone/Balkone_try.jpeg",
     "img/balkone/Balkone_black.jpeg",
-    "img/balkone/Balkone_kbir.jpeg",
+    "img/balkone/balkone_kbir.jpeg",
     "img/balkone/balkone_mzyan.jpeg",
 
     // // --- CARPORTS (4 images) ---
